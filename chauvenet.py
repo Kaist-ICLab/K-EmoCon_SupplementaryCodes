b7e12f3ff3fe7ad9048c1a8019d63d536776b07a
Cheul Young Park <cheulyop@gmail.com>
2020-05-08 01:48:50 +0900

    Add files via upload

---
+++ chauvenet.py
@@ -0,0 +1,25 @@
+from scipy import special
+
+def criterion(data):
+    '''
+    Implements Chauvenet's criterion for outlier detection.
+    
+    * See: https://en.wikipedia.org/wiki/Chauvenet%27s_criterion
+    
+    Parameters
+    ----------
+    data: Pandas Dataframe or named Series
+    
+    Returns
+    -------
+    A masked object of same shape and type as data, with Trues in where input 
+    values were outliers, and Falses in where values were not outliers.
+    
+    '''
+
+    mean, std, N = data.mean(), data.std(), len(data)  # mean, standard deviation, and length of input data
+    criterion = 1.0 / (2 * N)                          # Chauvenet's criterion
+    d = abs(data - mean) / std                         # distance of a value to mean in stdev.'s
+    prob = special.erfc(d)                             # area of normal dist.
+    
+    return prob < criterion                            # if prob is below criterion, a value is an outlier
