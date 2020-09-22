7990617da6a045d9e7fcbc9a9bda4bff0c2bae1c
Cheul Young Park <cheulyop@gmail.com>
2020-09-22 12:37:48 +0000

    updated logging

--- utils/logging.py
+++ utils/logging.py
@@ -1,8 +1,34 @@
 import time
 import pytz
+import logging
 from datetime import datetime
 
 
 def ltnow(*args, tz='Asia/Seoul'):
     '''Returns a formatted string of local time now given a string for timezone.'''
     return pytz.utc.localize(datetime.utcnow()).astimezone(pytz.timezone(tz)).timetuple()
+
+
+def init_logger(name='default'):
+    # create logger
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+
+    # create console handler and set level to debug
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.DEBUG)
+
+    # create formatter
+    formatter = logging.Formatter(
+        fmt='[%(asctime)s] %(levelname)s - %(message)s',
+        datefmt='%Y/%m/%d %H:%M:%S',
+    )
+    formatter.converter = ltnow
+
+    # add formatter to ch
+    ch.setFormatter(formatter)
+
+    # add ch to logger
+    logger.addHandler(ch)
+
+    return logger
